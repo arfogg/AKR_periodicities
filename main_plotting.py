0f4a88e642d28cf1b9a8e8878a5c4776cbde326e
Alexandra Ruth Fogg <arfogg@cp.dias.ie>
2024-04-05 16:13:12 +0100

    interpolating / resampling intensity to even resolution

--- main_plotting.py
+++ main_plotting.py
@@ -10,6 +10,7 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import scipy.interpolate as interpolate
 
 sys.path.insert(1, r'C:\Users\admin\Documents\wind_waves_akr_code\wind_utility')
 import read_integrated_power
@@ -20,7 +21,7 @@ def paper_plots():
     
     # ----- Read in AKR intensity data -----
     years=np.array([1995,1996,1997,1998,1999,2000,2001,2002,2003,2004])
-    intensity_df=read_integrated_power.concat_integrated_power_years(years, 'waters')
+    input_df=read_integrated_power.concat_integrated_power_years(years, 'waters')
     
     
     # ----- END -----
@@ -29,6 +30,27 @@ def paper_plots():
     # NEED TO RESAMPLE/INTERPOLATE TO MAKE THE DATA AN EVEN TEMPORAL RESOLUTION
     #   SO THE FFT WORKS NICELY
     
+    # ----- Resample temporally -----
+    # Interpolate / resample the AKR intensity data so it's on an even
+    #   temporal resolution of 3 minutes exactly.
+    
+    s_time=input_df.datetime_ut.iloc[0].ceil(freq='min')
+    e_time=input_df.datetime_ut.iloc[-1].floor(freq='min')
+    n_periods=np.floor((e_time-s_time)/pd.Timedelta(minutes=3))
+    new_time_axis=pd.date_range(s_time, periods=n_periods, freq='3T')
+    unix_time_axis=(new_time_axis - pd.Timestamp('1970-01-01')) / (pd.Timedelta(seconds=1))
+    intensity_df=pd.DataFrame({'datetime_ut':new_time_axis,
+                               'unix':unix_time_axis})
+    
+    func=interpolate.interp1d(input_df.unix,input_df['P_Wsr-1_100_650_kHz'])
+    
+    
+
+    intensity_df['P_Wsr-1_100_650_kHz']=func(unix_time_axis) 
+    return intensity_df
+
+    
+    # ----- END -----
     
     # ----- FFT -----
     
@@ -59,7 +81,7 @@ def test_dataset():
 def fast_fourier_transform(intensity_df):
     
     sp=np.fft.fft(intensity_df['P_Wsr-1_100_650_kHz'])
-    freq=np.fft.fftfreq(np.array(intensity_df['P_Wsr-1_100_650_kHz']).shape[-1])
+    freq=np.fft.fftfreq(np.array(intensity_df['P_Wsr-1_100_650_kHz']).shape[-1], d=(3.*60.))
     fig,ax=plt.subplots()
     ax.plot(freq, sp.real, label='real')
     ax.plot(freq, sp.imag, label='imag')
