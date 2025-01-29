37fa168b1278bd65ae2de4a0c3371a613d452b6a
Alexandra Ruth Fogg <arfogg@cp.dias.ie>
2025-01-29 11:49:30 +0000

    comments, docstring, pep 8 for diurnal_oscillator.py

--- diurnal_oscillator.py
+++ diurnal_oscillator.py
@@ -16,7 +16,6 @@ import matplotlib.pyplot as plt
 from neurodsp.sim import sim_oscillation, sim_powerlaw
 from neurodsp.sim.utils import modulate_signal
 
-
 alphabet = list(string.ascii_lowercase)
 axes_labels = []
 for a in alphabet:
@@ -76,10 +75,10 @@ def oscillating_signal(osc_freq, add_noise=True, noise_level=0.1,
         Signal.
 
     """
+
     # Create fake time axis
     yr_secs = 365*24*60*60    # One year in seconds
     res_secs = 3*60   # Temporal resolution of 3 minutes
-
     time = np.arange(0, yr_secs, res_secs)
 
     akr_osc = sim_oscillation(n_seconds=yr_secs,
@@ -89,8 +88,11 @@ def oscillating_signal(osc_freq, add_noise=True, noise_level=0.1,
                               std=osc_freq * 6 * 60)
     akr_osc = (akr_osc + 5.0) * 1e6
     # ^ make positive and put to the order of AKR power
+
+    # Store this unaltered oscillation
     clean = akr_osc
 
+    # Add noise to the simulation
     if add_noise:
         noisy = akr_osc + np.random.normal(loc=noise_level*np.mean(akr_osc),
                                            scale=noise_level *
@@ -98,6 +100,7 @@ def oscillating_signal(osc_freq, add_noise=True, noise_level=0.1,
                                            size=akr_osc.size)
         akr_osc = noisy
 
+    # Add an amplitude modulation to the simulation
     if add_amplitude_modulation:
         # Simulate a different modulating signal, this time
         modulation = np.abs(sim_powerlaw(n_seconds=yr_secs,
@@ -107,18 +110,23 @@ def oscillating_signal(osc_freq, add_noise=True, noise_level=0.1,
         amplitude_modulated = modulate_signal(akr_osc, modulation)
         akr_osc = amplitude_modulated
 
+    # Add data gaps to the simulation
     if create_data_gaps:
         # The average length of the data gaps
         #   (in terms of number of data points)
         extract_length = 20  # 20 data points -> 60 minutes
+
         # Number of synthetic data gaps
         #   (approx 2 per day)
         n_gaps = int((yr_secs/(24*60*60))*2)
+        # Choose random start points for gaps
         start_point = np.random.choice(akr_osc.size, size=n_gaps)
+        # Choose end points
         end_point = start_point + np.random.geometric(1.0 / extract_length,
                                                       size=n_gaps)
-
         synthetic_gaps = akr_osc.copy()
+
+        # For each gap, replace amplitude with NaN
         for i in range(n_gaps):
             if end_point[i] < len(akr_osc):
                 synthetic_gaps[start_point[i]:end_point[i]] = np.nan
@@ -126,7 +134,9 @@ def oscillating_signal(osc_freq, add_noise=True, noise_level=0.1,
                 synthetic_gaps[start_point[i]:] = np.nan
         akr_osc = synthetic_gaps.copy()
 
+    # Create summary plot
     if plot:
+        # --- Timeseries ---
         fig, ax = plt.subplots(1, 2,
                                gridspec_kw={'width_ratios': [2, 1]},
                                figsize=(12, 5))
@@ -140,6 +150,7 @@ def oscillating_signal(osc_freq, add_noise=True, noise_level=0.1,
         yval = clean[yi]
 
         if add_noise:
+            # Plot noisy signal
             ax[0].plot(time, noisy, linewidth=1.0,
                        label='Random Gaussian Noise',
                        color=noise_color, zorder=1.5)
@@ -164,6 +175,7 @@ def oscillating_signal(osc_freq, add_noise=True, noise_level=0.1,
             mod_ax.spines['right'].set_color(modf_color)
 
         if create_data_gaps:
+            # Plot with missing data
             ax[0].plot(time, synthetic_gaps + 1E6, linewidth=1.0,
                        label='Synthetic Gaps\n(amplitude + 1E6)',
                        color=gaps_color, zorder=1.75)
@@ -179,9 +191,11 @@ def oscillating_signal(osc_freq, add_noise=True, noise_level=0.1,
         ax[0].set_xlim(0, xmax)
         ax[0].set_ylim(top=1.1*np.nanmax(yval))
 
+        # Label axes
         ax[0].set_xlabel('Time (hrs)', fontsize=fontsize)
         ax[0].set_ylabel('Signal Amplitude', fontsize=fontsize)
 
+        # Format x ticks
         ticks_in_secs = []
         ticks_in_hrs = []
         for i in range(n_plot_osc):
@@ -191,16 +205,20 @@ def oscillating_signal(osc_freq, add_noise=True, noise_level=0.1,
             ticks_in_hrs.append(str(i * osc_freq))
         ax[0].set_xticks(ticks_in_secs, ticks_in_hrs)
 
+        # Create legend
         leg_ln = [*ax[0].get_legend_handles_labels()[0], mod_leg[0]]
         leg_lab = [*ax[0].get_legend_handles_labels()[1],
                    mod_leg[0].get_label()]
         ax[0].legend(leg_ln, leg_lab, fontsize=0.65*fontsize,
                      loc='lower center', ncol=3)
 
-        # Histogram
+        # --- Histogram ---
+        # Define histogram bins
         hist_max = np.nanmax([np.nanmax(clean), np.nanmax(noisy),
                               np.nanmax(amplitude_modulated)])
         bins = np.linspace(0, hist_max, 25)
+
+        # Histogram of different signals
         ax[1].hist(clean, color='black', alpha=0.5, bins=bins,
                    edgecolor='black', label='Pure oscillator')
         ax[1].hist(noisy, color=noise_color, alpha=0.5, bins=bins,
@@ -211,13 +229,13 @@ def oscillating_signal(osc_freq, add_noise=True, noise_level=0.1,
         ax[1].hist(synthetic_gaps, facecolor='none', bins=bins,
                    edgecolor='black', hatch='x', label='Synthetic\nGaps')
 
+        # Label axes
         ax[1].set_xlabel('Signal Amplitude', fontsize=fontsize)
         ax[1].set_ylabel('Occurrence', fontsize=fontsize)
 
         # Modulation factor histgram
         mod_hax = ax[1].twiny()
         mod_bins = np.linspace(0, np.nanmax(modulation), 25)
-
         mod_hax.hist(modulation, color=modf_color, alpha=0.5, bins=mod_bins,
                      histtype='step', linewidth=2., label='Modulation\nfactor')
         ax[1].hist([], color=modf_color, alpha=0.5, bins=mod_bins,
@@ -228,8 +246,10 @@ def oscillating_signal(osc_freq, add_noise=True, noise_level=0.1,
                             labelsize=fontsize)
         mod_hax.spines['top'].set_color(modf_color)
 
+        # Legend
         ax[1].legend(fontsize=0.65*fontsize)
 
+        # Formatting
         for i, a in enumerate(ax):
             a.tick_params(labelsize=fontsize)
 
