bug fixing and visualisation updates to binning averaging

binning_averaging.py
@@ -103,9 +103,14 @@ def return_UT_trend(data_df, region_centres=[0, 6, 12, 18],
 
     UT_bins = np.linspace(0, 24-UT_bin_width, int(24/UT_bin_width)) +\
         (UT_bin_width/2)
-    data_df['decimal_hr'] = (data_df.datetime -
-                             data_df.datetime.values.astype('datetime64[D]')).\
-        astype('timedelta64[m]') / 60.
+    # data_df['decimal_hr'] = (data_df.datetime -
+    #                          data_df.datetime.values.astype('datetime64[D]')).\
+    #     astype('timedelta64[m]') / 60.
+    # Calculate the decimal hour of the observation in UT
+    data_df['decimal_hr'] = ((data_df['datetime'] -
+                              data_df['datetime'].dt.normalize()) 
+                             / pd.Timedelta(hours=1))
+
 
     UT_df = pd.DataFrame({'UT_bin_centre': UT_bins})
 
@@ -117,12 +122,17 @@ def return_UT_trend(data_df, region_centres=[0, 6, 12, 18],
 
         UT_median = np.full(UT_bins.size, np.nan)
         UT_mad = np.full(UT_bins.size, np.nan)
-        UT_dist = []
+        
+        UT_median_no0 = np.full(UT_bins.size, np.nan)
+        UT_mad_no0 = np.full(UT_bins.size, np.nan)
+        # UT_dist = []
 
         UT_n = np.full(UT_bins.size, np.nan)
+        UT_n_no0 = np.full(UT_bins.size, np.nan)
 
         for j in range(UT_bins.size):
-
+            print(i, c, n, f)
+            print(j, UT_bins[j], UT_bin_width)
             UT_ind, = np.where((LT_data_df.decimal_hr >=
                                 (UT_bins[j]-UT_bin_width/2))
                                & (LT_data_df.decimal_hr <
@@ -131,15 +141,29 @@ def return_UT_trend(data_df, region_centres=[0, 6, 12, 18],
                 LT_data_df[ipower_tag].iloc[UT_ind].values)
             UT_mad[j], UT_median[j] = statistical_metrics.\
                 median_absolute_deviation(dist_)
+            UT_mad_no0[j], UT_median_no0[j] = statistical_metrics.\
+                median_absolute_deviation(dist_[dist_>0.])
 
             UT_n[j] = dist_.size
+            UT_n_no0[j] = dist_[dist_>0.].size
 
-            UT_dist.append(dist_)
+
+            # UT_dist.append(dist_)
+            # if n== 'dawn':
+            #     breakpoint()
 
         UT_df[n + '_median'] = UT_median
         UT_df[n + '_median_norm'] = UT_median / np.nanmax(UT_median)
         UT_df[n + '_mad'] = UT_mad
+        UT_df[n + '_mad_norm'] = UT_mad / np.nanmax(UT_median)
+
+        UT_df[n + '_median_no0'] = UT_median_no0
+        UT_df[n + '_median_norm_no0'] = UT_median_no0 / np.nanmax(UT_median_no0)
+        UT_df[n + '_mad_no0'] = UT_mad_no0
+        UT_df[n + '_mad_norm_no0'] = UT_mad_no0 / np.nanmax(UT_median_no0)    
+
         UT_df[n + 'n'] = UT_n
+        UT_df[n + 'n_no0'] = UT_n_no0
 
     return UT_df
 

main_plotting.py
@@ -711,14 +711,27 @@ def run_ACF():
     # fig.savefig(ACF_fig)
 
 
-def run_MLT_binning():
+def run_MLT_binning_overlayed(n_mlt_sectors='four'):
 
-    # Initialise variables
-    region_centres = [0, 6, 12, 18]
-    region_width = 6
-    region_names = ['midn', 'dawn', 'noon', 'dusk']
-    region_flags = [0, 1, 2, 3]
-    region_colors = ['grey', "#8d75ca", "#a39143", "#bb6c82"]
+
+    if n_mlt_sectors=='four':
+        # Initialise variables
+        region_centres = [0, 6, 12, 18]
+        region_width = 6
+        region_names = ['midn', 'dawn', 'noon', 'dusk']
+        region_mrkrs = ['o', '^', '*', 'x']
+        region_flags = [0, 1, 2, 3]
+        region_colors = ['grey', "#8d75ca", "#a39143", "#bb6c82"]
+    elif n_mlt_sectors=='eight':
+        # Initialise variables
+        region_centres = [0, 3, 6, 9, 12, 15, 18, 21]
+        region_width = 3
+        region_names = ['0', '3', '6', '9', '12', '15', '18', '21']
+        region_mrkrs = ['o', '^', '*', 'x', '+', 'X', "2", "."]
+        region_flags = [0, 1, 2, 3, 4, 5, 6, 7]
+        region_colors = ['grey', "#c18b40", "#a361c7", "#7ca343", "#6587cd",
+                         "#cc5643", "#49ae8a", "#c65c8a"]       
+
     UT_bin_width = 2
 
     # Different frequency channels
@@ -728,14 +741,15 @@ def run_MLT_binning():
     freq_labels = np.array(['100-400 kHz', '50-100 kHz'])
     freq_colors = np.array(['dimgrey', 'darkorange', 'rebeccapurple'])
 
-    MLT_fig = os.path.join(fig_dir, "three_interval_MLT_binned.png")
+    MLT_fig = os.path.join(fig_dir, "three_interval_MLT_binned_"+n_mlt_sectors+"MLT.png")
 
     # Read in interval data
     print('Reading AKR data over requested intervals')
     interval_options = read_and_tidy_data.return_test_intervals()
 
     # Initialise plotting window
-    fig, ax = plt.subplots(nrows=3, ncols=2, figsize=(21, 13))
+    fig, ax = plt.subplots(nrows=interval_options['tag'].size,
+                           ncols=freq_tags.size, figsize=(21, 13))
 
     # # Run ACF over the fake oscillator
     # ftime, fsignal = read_synthetic_oscillator()
@@ -768,7 +782,7 @@ def run_MLT_binning():
         print('Running autocorrelation for ', interval_tag)
 
         base_dir = pathlib.Path(data_dir) / 'MLT_binning'
-        file_paths = [base_dir / f"MLT_binned_{interval_tag}_{f}.csv" for f in freq_tags]
+        file_paths = [base_dir / f"MLT_binned_{n_mlt_sectors}sector_{interval_tag}_{f}.csv" for f in freq_tags]
         file_checks = [file_path.is_file() for file_path in file_paths]
 
         if all(file_checks) is False:
@@ -785,14 +799,15 @@ def run_MLT_binning():
         # Remove any rows where intensity == np.nan
         for (j, (freq_column, c, n)) in enumerate(zip(freq_tags, freq_colors, freq_labels)):
             print('Frequency band: ', freq_column)
-            MLT_csv = os.path.join(data_dir, 'MLT_binning', 'MLT_binned_' +
+            MLT_csv = os.path.join(data_dir, 'MLT_binning', 'MLT_binned_' + n_mlt_sectors +'sector_' +
                                   interval_tag + '_' + freq_column + '.csv')
 
             if pathlib.Path(MLT_csv).is_file() is False:
 
                 freq_df = akr_df.dropna(subset=[freq_column])
                 t1 = pd.Timestamp.now()
-                print('starting MLT binning at ', t1)              
+                print('starting MLT binning at ', t1)    
+                
                 UT_df = binning_averaging.return_UT_trend(
                         akr_df, region_centres=region_centres,
                         region_width=region_width, region_names=region_names,
@@ -809,15 +824,31 @@ def run_MLT_binning():
 
 
             if freq_column == 'ipwr_100_400kHz':
-                for k, (MLT_n, c) in enumerate(zip(region_names, region_colors)):
-                    ax[i, 0].plot(UT_df.UT_bin_centre, UT_df[MLT_n + '_median_norm'],
-                                  color=c, label=MLT_n)
+                for k, (MLT_n, c, mrkr) in enumerate(zip(region_names, region_colors,
+                                                   region_mrkrs)):
+                    ax[i, 0].plot(UT_df.UT_bin_centre, UT_df[MLT_n + '_median_norm_no0'],
+                                  color=c, label=MLT_n, marker=mrkr,
+                                  markersize=fontsize)
+                    ax[i, 0].fill_between(UT_df.UT_bin_centre,
+                                          UT_df[MLT_n + '_median_norm_no0'] -
+                                          UT_df[MLT_n + '_mad_norm_no0'],
+                                          UT_df[MLT_n + '_median_norm_no0'] +
+                                          UT_df[MLT_n + '_mad_norm_no0'],
+                                          color=c, alpha=0.2)
 
 
             elif freq_column == 'ipwr_50_100kHz':
-                for k, (MLT_n, c) in enumerate(zip(region_names, region_colors)):
-                    ax[i, 1].plot(UT_df.UT_bin_centre, UT_df[MLT_n + '_median_norm'],
-                                  color=c, label=MLT_n)
+                for k, (MLT_n, c, mrkr) in enumerate(zip(region_names, region_colors,
+                                                   region_mrkrs)):
+                    ax[i, 1].plot(UT_df.UT_bin_centre, UT_df[MLT_n + '_median_norm_no0'],
+                                  color=c, label=MLT_n, marker=mrkr,
+                                  markersize=fontsize)
+                    ax[i, 1].fill_between(UT_df.UT_bin_centre,
+                                          UT_df[MLT_n + '_median_norm_no0'] -
+                                          UT_df[MLT_n + '_mad_norm_no0'],
+                                          UT_df[MLT_n + '_median_norm_no0'] +
+                                          UT_df[MLT_n + '_mad_norm_no0'],
+                                          color=c, alpha=0.2)
 
             # Formatting
             ax[i, j].text(0.5, 1.01,
@@ -839,6 +870,167 @@ def run_MLT_binning():
 
     # Save to file
     fig.savefig(MLT_fig)
+
+
+def run_MLT_binning_seperate(n_mlt_sectors='four'):
+
+    fontsize = 20
+
+    if n_mlt_sectors == 'four':
+        # Initialise variables
+        region_centres = [0, 6, 12, 18]
+        region_width = 6
+        region_names = ['midn', 'dawn', 'noon', 'dusk']
+        region_mrkrs = ['o', '^', '*', 'x']
+        region_flags = [0, 1, 2, 3]
+        region_colors = ['grey', "#8d75ca", "#a39143", "#bb6c82"]
+    elif n_mlt_sectors == 'eight':
+        # Initialise variables
+        region_centres = [0, 3, 6, 9, 12, 15, 18, 21]
+        region_width = 3
+        region_names = ['0', '3', '6', '9', '12', '15', '18', '21']
+        region_mrkrs = ['o', '^', '*', 'x', '+', 'X', "2", "."]
+        region_flags = [0, 1, 2, 3, 4, 5, 6, 7]
+        region_colors = ['grey', "#c18b40", "#a361c7", "#7ca343", "#6587cd",
+                         "#cc5643", "#49ae8a", "#c65c8a"]
+
+    # UT bin is the same always
+    UT_bin_width = 2
+
+    # Different frequency channels
+    freq_tags = np.array(['ipwr_100_400kHz', 'ipwr_50_100kHz'])
+    freq_labels = np.array(['100-400 kHz', '50-100 kHz'])
+    freq_colors = np.array(['dimgrey', 'darkorange', 'rebeccapurple'])
+
+    # Read in interval data
+    print('Reading AKR data over requested intervals')
+    interval_options = read_and_tidy_data.return_test_intervals()
+
+    # Loop through different intervals / datasets
+    for (i, interval_tag) in enumerate(interval_options['tag']):
+        print('Running autocorrelation for ', interval_tag)
+
+        base_dir = pathlib.Path(data_dir) / 'MLT_binning'
+        file_paths = [base_dir /
+                      f"MLT_binned_{n_mlt_sectors}sector_{interval_tag}_{f}.csv"
+                      for f in freq_tags]
+        file_checks = [file_path.is_file() for file_path in file_paths]
+
+        if all(file_checks) is False:
+            # Read in AKR intensity data
+            akr_df = read_and_tidy_data.select_akr_intervals(interval_tag)
+            mlt_flag, mlt_name = binning_averaging.calc_LT_flag(
+                akr_df, region_centres=region_centres,
+                region_width=region_width, region_names=region_names,
+                region_flags=region_flags)
+            akr_df['mlt_flag'] = mlt_flag
+            akr_df['mlt_name'] = mlt_name
+
+        # Loop through different frequency columns
+        for (j, (freq_column, c, n)) in enumerate(zip(freq_tags,
+                                                      freq_colors,
+                                                      freq_labels)):
+            print('Frequency band: ', freq_column)
+            MLT_csv = os.path.join(data_dir, 'MLT_binning',
+                                   'MLT_binned_' + n_mlt_sectors + 'sector_' +
+                                   interval_tag + '_' + freq_column + '.csv')
+
+            # Initialise plotting window
+            fig, ax = plt.subplots(nrows=len(region_centres), ncols=1,
+                                   figsize=(15, len(region_centres) * 3.))
+            fig_name = os.path.join(fig_dir, interval_tag + "_" + freq_column
+                                    + "_" + n_mlt_sectors + "MLT.png")
+
+            # Bin data into UT bins
+            if pathlib.Path(MLT_csv).is_file() is False:
+
+                # freq_df = akr_df.dropna(subset=[freq_column])
+                # t1 = pd.Timestamp.now()
+                # print('starting MLT binning at ', t1)
+                UT_df = binning_averaging.return_UT_trend(
+                        akr_df, region_centres=region_centres,
+                        region_width=region_width, region_names=region_names,
+                        region_flags=region_flags, UT_bin_width=UT_bin_width,
+                        ipower_tag=freq_column)
+                # t2 = pd.Timestamp.now()
+                # print('MLT binning finished, time elapsed: ', t2-t1)
+                UT_df.to_csv(MLT_csv, index=False)
+            # Else read in pre-sorted data
+            else:
+                UT_df = pd.read_csv(MLT_csv, delimiter=',',
+                                    float_precision='round_trip')
+
+            # Loop through each MLT sector, plotting
+            for k, (MLT_n, c, mrkr) in enumerate(zip(region_names,
+                                                     region_colors,
+                                                     region_mrkrs)):
+                # Set x axis limits
+                ax[k].set_xlim(left=0., right=24.)
+
+                # Plot number of observations
+                ax[k].bar(UT_df.UT_bin_centre, UT_df[MLT_n + "n"],
+                          zorder=0.5, color='gold', linewidth=.75,
+                          edgecolor='black', alpha=0.75, label='# all')
+                ax[k].bar(UT_df.UT_bin_centre, UT_df[MLT_n + "n_no0"],
+                          zorder=0.6, color='deeppink', linewidth=.75,
+                          edgecolor='black', alpha=0.75, label="# > 0")
+
+                # Create twin axis
+                twax = ax[k].twinx()
+
+                # Plot intensity trend
+                twax.plot(UT_df.UT_bin_centre,
+                          UT_df[MLT_n + '_median_norm_no0'],
+                          color='black', label="median", marker='o',
+                          markersize=fontsize, linewidth=1.5, zorder=2.5)
+                twax.fill_between(UT_df.UT_bin_centre,
+                                  UT_df[MLT_n + '_median_norm_no0'] -
+                                  UT_df[MLT_n + '_mad_norm_no0'],
+                                  UT_df[MLT_n + '_median_norm_no0'] +
+                                  UT_df[MLT_n + '_mad_norm_no0'],
+                                  color='grey', alpha=0.5, label="MAD",
+                                  zorder=2)
+
+                # Formatting
+                # Panel MLT sector
+                t = ax[k].text(0.98, 0.95, MLT_n + ' MLT',
+                               transform=ax[k].transAxes, fontsize=fontsize,
+                               va='top', ha='right')
+                t.set_bbox(dict(facecolor='white', alpha=0.75,
+                                edgecolor='grey'))
+                # Y labels
+                ax[k].set_ylabel("# averaged", fontsize=fontsize)
+                twax.set_ylabel('Normalised median\nintegrated power',
+                                fontsize=fontsize)
+                # Fontsize
+                ax[k].tick_params(labelsize=fontsize)
+                # Panel label
+                t = ax[k].text(0.02, 0.95, axes_labels[k],
+                               transform=ax[k].transAxes, fontsize=fontsize,
+                               va='top', ha='left')
+                t.set_bbox(dict(facecolor='white', alpha=0.75,
+                                edgecolor='grey'))
+
+            # More formatting
+            ax[0].text(0.5, 1.01,
+                       interval_options.label[i] + ' (' + n + ')',
+                       transform=ax[0].transAxes,
+                       fontsize=1.25 * fontsize, va='bottom', ha='center')
+            leg_ln = [*ax[k].get_legend_handles_labels()[0],
+                      *twax.get_legend_handles_labels()[0]]
+            leg_lab = [*ax[k].get_legend_handles_labels()[1],
+                       *twax.get_legend_handles_labels()[1]]
+            # ax[0].legend(leg_ln, leg_lab, fontsize=0.65*fontsize,
+            #              loc='lower center', ncol=3)
+            ax[0].legend(leg_ln, leg_lab, bbox_to_anchor=(1.13, 0.0, 0.2, 1.0),
+                         loc="center left", fontsize=fontsize)
+            ax[k].set_xlabel('UT (hours)', fontsize=fontsize)
+
+            fig.tight_layout()
+
+            # Save to file
+            # fig.savefig(fig_name)
+            return
 
 
 