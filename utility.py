5278175671fc376455fd850c82dfc8b64d144a07
Alexandra Fogg <arfogg@cp.dias.ie>
2025-08-26 10:05:05 +0100

    tidying, docstrings and PEP8 formatting for utility.py

--- utility.py
+++ utility.py
@@ -26,6 +26,7 @@ fig_dir = os.path.join("C:" + os.sep,
 data_dir = os.path.join(fig_dir, "data_quickloads")
 wu_dir = os.path.join(fig_dir, "SW_data")
 
+
 def interpolate_mlt(desired_timestamps, data_df, mlt_flag='decimal_gseLT'):
     """
     Interpolate magnetic local time / MLT. Requires special
@@ -67,7 +68,22 @@ def interpolate_mlt(desired_timestamps, data_df, mlt_flag='decimal_gseLT'):
 
 
 def generate_random_phase_surrogate(data, plot=False):
+    """
+    Generate a random phase surrogate.
 
+    Parameters
+    ----------
+    data : np.array
+        Data to create a surrogate for.
+    plot : BOOL, optional
+        If True, a summary plot is created. The default is False.
+
+    Returns
+    -------
+    surrogate : np.array
+        Random phase surrogate.
+
+    """
     print('Generating Random Phase Surrogate')
     surrogate = aaft.AAFTsur(data)
 
@@ -100,73 +116,81 @@ def generate_random_phase_surrogate(data, plot=False):
 
     return surrogate
 
-# def chunk_gse_to_geo(data):
-    
-#     print(data['datetime'][0], data['datetime'][len(data)-1])
-    
-# #     o_csv = os.path.join(data_dir, 'coord_transform')
-
-# def full_archive_geo_coord_parallel():
-    
-#     interval_options = read_and_tidy_data.return_test_intervals()    
-#     data = read_and_tidy_data.select_akr_intervals(interval_options['tag'][0])
-
-#     chunk_gse_to_geo(data)
 
 def convert_gse_to_geo(data, rad_tag='radius', lat_tag='lat_gse',
                        lon_tag='lon_gse', unix_tag='unix'):
-    # data is a DataFrame containing datetime, decimal_gseLT, lon_gse
-    # dim (n,3)
-    # rad, lat, lon
-    
-    # Check for output file
-    
+    """
+    Convert spacecraft GSE position to GEO.
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        Pandas DataFrame containing 'datetime', rad_tag, lat_tag,
+        lon_tag, unix_tag.
+    rad_tag : string, optional
+        DataFrame column for radial distance. The default is 'radius'.
+    lat_tag : string, optional
+        DataFrame column for latitude. The default is 'lat_gse'.
+    lon_tag : Tstring, optional
+        DataFrame column for longitude. The default is 'lon_gse'.
+    unix_tag : string, optional
+        DataFrame column for UNIX time. The default is 'unix'.
+
+    Returns
+    -------
+    geo_lat : np.array
+        Geographic latitude.
+    geo_lon : np.array
+        Geographic longitude.
+
+    """
+
+    # Reshape inputs for spacepy
     input_coord_array = np.array([data[rad_tag].values,
-                                        data[lat_tag].values,
-                                        data[lon_tag].values]).T
-    
+                                  data[lat_tag].values,
+                                  data[lon_tag].values]).T
+
     # Initialise coord class
-    # print('banana')
     coord_cls = coord.Coords(input_coord_array, 'GSE', 'sph')
-    
+
     # Fold in time
-    # print('apple')
     coord_cls.ticks = Ticktock(data[unix_tag].values, 'UNX')
-    
-    # from spacepy import coordinates as coord
-    # cvals = coord.Coords([[1,2,4],[1,2,2]], 'GEO', 'car')
-    # cvals.x  # returns all x coordinates
-    # from spacepy.time import Ticktock
-    # cvals.ticks = Ticktock(['2002-02-02T12:00:00', '2002-02-02T12:00:00'], 'ISO') # add ticks
-    # newcoord = cvals.convert('GSM', 'sph')
-    # newcoord
-    # print('guava')
+
+    # Convert, slow
     newcoord = coord_cls.convert('GEO', 'sph')
-    
+
     geo_lat = newcoord.lati
     geo_lon = newcoord.long
-    
+
     return geo_lat, geo_lon
 
-def full_archive_geo_coord():
 
-    interval_options = read_and_tidy_data.return_test_intervals()    
+def full_archive_geo_coord():
+    """
+    Run conversion for full archive
+
+    Returns
+    -------
+    out_df : pd.DataFrame
+        DataFrame containing the converted coordinates.
+
+    """
+
+    interval_options = read_and_tidy_data.return_test_intervals()
     data = read_and_tidy_data.select_akr_intervals(interval_options['tag'][0])
 
-    print('full archive len ', len(data))
-    #data = data.iloc[0:100000]
     geo_coords_csv = os.path.join(data_dir, "full_archive_GEO_coords.csv")
-    
+
+    # Check if already converted
     if pathlib.Path(geo_coords_csv).is_file():
         out_df = pd.read_csv(geo_coords_csv, delimiter=',',
                              float_precision='round_trip')
-        
-        
+
     else:
         print('Converting from GSE to GEO coordinates for full archive')
         t0 = pd.Timestamp.now()
         print('Started at:', t0)
-        # Run this over chunks, so that it's quicker
+        # Run this over chunks
         chunk_length = 10000
         n_loops = int(np.ceil(len(data)/chunk_length))
 
@@ -181,8 +205,7 @@ def full_archive_geo_coord():
             g_lat, g_lon = convert_gse_to_geo(data[starting_i:ending_i])
             geo_lat = np.append(geo_lat, g_lat)
             geo_lon = np.append(geo_lon, g_lon)
-        
-        # geo_lat, geo_lon = convert_gse_to_geo(data)
+
         t2 = pd.Timestamp.now()
         print('Ended at:', t2)
         print('Time elapsed:', t2-t0)
@@ -191,12 +214,29 @@ def full_archive_geo_coord():
                                'lon_geo': geo_lon,
                                'lat_geo': geo_lat})
         out_df.to_csv(geo_coords_csv, index=False)
-        
+
     return out_df
 
+
 def calc_longitude_of_sun(data, lon_tag='lon_gse', plot=False):
-    # data is a DataFrame containing datetime, decimal_gseLT, lon_tag
-    # NEED TO CHECK WITH SIYUAN ABOUT LON GSM OR GSE!!
+    """
+    Calculate the longitude of the Sun.
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        Pandas DataFrame containing 'datetime', lon_tag, 'decimal_gseLT'.
+    lon_tag : string, optional
+        DataFrame column for longitude. The default is 'lon_gse'.
+    plot : BOOL, optional
+        If True, a summary plot is created. The default is False.
+
+    Returns
+    -------
+    lon_sol : np.array
+        Longitude of the Sun.
+
+    """
 
     lon_sol = (data[lon_tag] % 360) - ((12. - data.decimal_gseLT) * 15.)
     lon_sol = lon_sol % 360
@@ -217,55 +257,57 @@ def calc_longitude_of_sun(data, lon_tag='lon_gse', plot=False):
 
 
 def read_wu_period(lon, f):
-    
-    # f= 'low' or 'high'
-    # lon= 'sc' or 'sun'
-    
-    # fnames = [os.path.join(wu_dir, "AKR_period_LongitudeSC_LSSA_test_30_100_kHz_v0627.mat")]
-    
-    # for f in fnames:
-    #     m = scipy.io.loadmat(f)
-    #     print(m.keys())
-    #     print(f)
-    #     breakpoint()
+    """
+    Function to read in the modulation spectrogram by SW.
+
+    Parameters
+    ----------
+    lon : string
+        For which longitude, valid options 'sc' or 'sun'.
+    f : string
+        For which frequency band, valid options 'low' or 'high'.
+
+    Returns
+    -------
+    out_dict : dictionary
+        Spectrogram data dictionary.
+
+    """
+
+    # Create filename
     if f == 'low':
         freq_str = '30_100'
     elif f == 'high':
         freq_str = '100_800'
-    
+
     if lon == 'sc':
         lon_str = 'LongitudeSC'
         spec_str = 'spec_LongitudeofSC'
     elif lon == 'sun':
         lon_str = 'LongitudeSun'
         spec_str = 'spec_LongitudeofSun'
-    
 
     f = os.path.join(wu_dir, "AKR_period_" + lon_str + "_LSSA_test_" + freq_str + "_kHz_v0627.mat")
-    # f = os.path.join(wu_dir, "AKR_period_LongitudeSC_LSSA_test_30_100_kHz_v0627.mat")
-    
-    
-    
-    m = scipy.io.loadmat(f)
-    
-    # print(m.keys())
-    
-    days_since_0000 = m['time'].flatten()
-    
-    days_since_1990 = days_since_0000 - ((pd.Timestamp(1990,1,1,0) - pd.Timestamp(1,1,1,0)).days + 1 + 365)
 
+    # Read in file
+    m = scipy.io.loadmat(f)
+
+    # Transform time
+    days_since_0000 = m['time'].flatten()
+    days_since_1990 = days_since_0000 - (
+        (pd.Timestamp(1990, 1, 1, 0) -
+         pd.Timestamp(1, 1, 1, 0)).days + 1 + 365)
     timestamps = []
     for d in days_since_1990:
-        timestamps.append(pd.Timedelta(days = d) + pd.Timestamp(1990,1,1,0))
-    
-    
-    #m['f_inDegreePerDay'].flatten() * (1/360) / 86400 # freq in hertz
-    #1 / (m['f_inDegreePerDay'].flatten() * (1/360) / 86400) # period in seconds
-    period_hrs = (1 / (m['f_inDegreePerDay'].flatten() * (1/360) / 86400)) / (60*60) # period in hours
-    
+        timestamps.append(pd.Timedelta(days=d) + pd.Timestamp(1990, 1, 1, 0))
+
+    # Transform frequency to period
+    period_hrs = (1 / (m['f_inDegreePerDay'].flatten() * (1 / 360) / 86400)) / (60 * 60) # period in hours
+
+    # Create output dictionary
     out_dict = {'timestamp': timestamps,
                 'period_hours': period_hrs,
                 'freq_deg_per_day': m['f_inDegreePerDay'].flatten(),
                 'spectrogram': m[spec_str]}
-    
+
     return out_dict
